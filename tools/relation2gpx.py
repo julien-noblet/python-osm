83e91ce1e63d3c47dc78d095de4bfba65a5e29f5
Werner Hoch <werner.ho@gmx.de>
2009-10-07 15:25:36 +0200

    Command line interface to export a list of relations

--- tools/relation2gpx.py
+++ tools/relation2gpx.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
 
-import sys
+import sys, os
 import xml.dom.minidom
 
-sys.path.append('../')
+REL_DIR= "../"       # relation between this script and the BASE_DIR
+BASE_DIR = os.path.join(os.getcwd(),
+                        os.path.dirname(sys.argv[0]),
+                        REL_DIR)
+sys.path.append(BASE_DIR)
 import pyosm
 
 
@@ -28,32 +32,26 @@ class osm_gpx_exporter(object):
         self.gpx_dom = xml.dom.minidom.parseString(self.gpx_template)
         self.gpx_root = self.gpx_dom.documentElement
     
-    def append_osm(self, osm, relation=None, way=None, node=None):
+    def append_relations(self, relations=[], recursive=True):
         """
         append an osm object to a gpx track
         relation --> track (trk)
         way --> track segment (trkseg)
         node --> track point (trkpt)
         """
-        if relation != None:
-            pass
-        elif way != None:
-            pass
-        elif node != None:
-            pass
-        else:
-            ways = osm.ways
-        
-        trk = self.gpx_dom.createElement('trk')
-        self.gpx_root.appendChild(trk)
-        for way in ways:
-            trkseg = self.gpx_dom.createElement('trkseg')
-            trk.appendChild(trkseg)
-            for node in way.nodes:
-                trkpt = self.gpx_dom.createElement('trkpt')
-                trkpt.setAttribute('lat', node.lat)
-                trkpt.setAttribute('lon', node.lon)
-                trkseg.appendChild(trkpt)
+        for rel in relations:
+            trk = self.gpx_dom.createElement('trk')
+            self.gpx_root.appendChild(trk)
+            for m in rel.members:
+                if type(m) != pyosm.Way:
+                    continue
+                trkseg = self.gpx_dom.createElement('trkseg')
+                trk.appendChild(trkseg)
+                for node in m.nodes:
+                    trkpt = self.gpx_dom.createElement('trkpt')
+                    trkpt.setAttribute('lat', node.lat)
+                    trkpt.setAttribute('lon', node.lon)
+                    trkseg.appendChild(trkpt)
 
     def write(self):
         """
@@ -63,10 +61,34 @@ class osm_gpx_exporter(object):
                 
                 
 if __name__ == '__main__':
-    osm = pyosm.OSMXMLFile('relation157307.osm')
-    osm.statistic()
-    gpx_exp = osm_gpx_exporter('out.gpx')
-    gpx_exp.append_osm(osm)
-    gpx_exp.write()
+    import getopt
+    import urllib
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'o:r',
+                                   ['outfile=', 'relations'])
+    except getopt.GetoptError:
+        usage()
+        sys.exit()
+
+    outfile='out.gpx'
+    mode = None
+
+    for o, a in opts:
+        if o in ['-o', '--outfile']:
+            outfile = a
+        elif o in ['-r', 'relations']:
+            mode = 'relations'
+
+    if mode == 'relations':
+        API='http://www.openstreetmap.org/api/0.6'
+        gpx_exp = osm_gpx_exporter(outfile)
+        
+        for relid in args:
+            osm = urllib.urlretrieve('%s/relation/%s/full' %(API,relid), 'temporary.osm')
+            osmobj = pyosm.OSMXMLFile('temporary.osm')
+            gpx_exp.append_relations(osmobj.relations)
+            
+        gpx_exp.write()
 
                        

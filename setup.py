789a44771f9cad2bd51a27ccb37ff13234ff80fb
Denis Klimov <zver@altlinux.org>
2010-04-08 17:31:11 +0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+    name = "python-osm", 
+    version = "0.0.1", 
+    description = "OpenStreetMap library for python ",
+    scripts = [
+             'tools/osmhistory.py', 
+             'tools/relation2gpx.py'
+    ], 
+    py_modules = [
+             'osm.pyosm',
+             'osm.multipolygon',
+    ],
+    package_dir = {'osm': 'src'},
+)
+

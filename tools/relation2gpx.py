9f5f55103ecf8acbf36f6dba5b06b25fbf0ccd0f
Werner Hoch <werner.ho@gmx.de>
2011-12-18 10:12:38 +0100

    improved CLI of relation2gpx.py, added documentation

--- tools/relation2gpx.py
+++ tools/relation2gpx.py
@@ -7,7 +7,7 @@ PYOSM_DIR = os.path.join(os.path.dirname(__file__), '../src')
 sys.path.append(PYOSM_DIR)
 from osm import pyosm
 
-
+#################### CLASSES
 class osm_gpx_exporter(object):
     gpx_template = """<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
     <gpx xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1"
@@ -53,37 +53,53 @@ class osm_gpx_exporter(object):
         export the gpx_dom into a file
         """
         open(self.gpx_filename,'wt').write(self.gpx_dom.toprettyxml("  "))
+
+
+#################### FUNCTIONS
+def usage():
+    print "usage: relation2gpx.py [-o GPXFILE] -r relation1,relation2,..."
+    print "load relations from the OSM API and create gpx files"
+    print "  -h, --help: print this help message"
+    print "  -o, --outfile: specify the filename of the gpx file"
+    print "  -r, --relations: commaseperated list of relations"
+
                 
-                
+#################### MAIN
 if __name__ == '__main__':
     import getopt
     import urllib
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'o:r',
-                                   ['outfile=', 'relations'])
+        opts, args = getopt.getopt(sys.argv[1:], 'o:r:',
+                                   ['outfile=', 'relations='])
     except getopt.GetoptError:
         usage()
-        sys.exit()
+        sys.exit(1)
 
     outfile='out.gpx'
-    mode = None
+    relids = []
 
     for o, a in opts:
         if o in ['-o', '--outfile']:
             outfile = a
-        elif o in ['-r', 'relations']:
-            mode = 'relations'
+        elif o in ['-r', '--relations']:
+            relids = a.split(',')
+        elif o in ['-h', '--help']:
+            usage()
+            sys.exit()
 
-    if mode == 'relations':
-        API='http://www.openstreetmap.org/api/0.6'
-        gpx_exp = osm_gpx_exporter(outfile)
-        
-        for relid in args:
-            osmfile = urllib.urlopen('%s/relation/%s/full' %(API,relid))
-            osmobj = pyosm.OSMXMLFile(osmfile)
-            gpx_exp.append_relations(osmobj.relations.values())
-            
-        gpx_exp.write()
+    if not relids:
+        usage()
+        sys.exit(1)
+
+    API='http://www.openstreetmap.org/api/0.6'
+    gpx_exp = osm_gpx_exporter(outfile)
+
+    for relid in relids:
+        osmfile = urllib.urlopen('%s/relation/%s/full' %(API,relid))
+        osmobj = pyosm.OSMXMLFile(osmfile)
+        gpx_exp.append_relations(osmobj.relations.values())
+
+    gpx_exp.write()
 
                        
